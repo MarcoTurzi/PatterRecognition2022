ef7e13cb760b944de7eef3374d04fe389fb16b31
LukasJGraff <115481868+LukasJGraff@users.noreply.github.com>
2022-11-29 12:08:09 +0100

    Fixed error

--- training.py
+++ training.py
@@ -4,13 +4,11 @@ import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
 from sklearn.preprocessing import scale, StandardScaler
 
-
-
 df = pd.read_csv("./data/mnist.csv")
 df_values = df.values
 print(df_values)
-labels = df_values[:,0]
-digits = df_values[:,1:]
+labels = df_values[:, 0]
+digits = df_values[:, 1:]
 img_size = 28
 '''print(digits) 
 plt.imshow(digits[1].reshape(img_size,img_size))
@@ -38,20 +36,21 @@ def ink_features(values):
         ink = np.append(ink, ink_v)
     return ink
 
+
 ink_f = ink_features(df_values)
-ink_mean = [np.round(np.mean(ink_f[labels == i]) ,2) for i in range(10)]
+ink_mean = [np.round(np.mean(ink_f[labels == i]), 2) for i in range(10)]
 print(ink_mean)
-ink_std = [np.round(np.std(ink_f[labels == i]),2) for i in range(10)]
+ink_std = [np.round(np.std(ink_f[labels == i]), 2) for i in range(10)]
 print(ink_std)
 
-scaler= StandardScaler()
+scaler = StandardScaler()
 digits_scaled = scaler.fit_transform(digits)
 '''ink_f_scaled = np.array([np.sum(row) for row in digits])
 '''
 ink_f_scaled = scale(ink_f)
-ink_mean = [np.round(np.mean(ink_f_scaled[labels == i]) ,2) for i in range(10)]
+ink_mean = [np.round(np.mean(ink_f_scaled[labels == i]), 2) for i in range(10)]
 print(ink_mean)
-ink_std = [np.round(np.std(ink_f_scaled[labels == i]),2) for i in range(10)]
+ink_std = [np.round(np.std(ink_f_scaled[labels == i]), 2) for i in range(10)]
 print(ink_std)
 
 
@@ -59,11 +58,11 @@ print(ink_std)
 def getVariances(pixData):
     vars = np.var(pixData, axis=0)
     print('Variance per pixel')
-    plt.imshow(vars.reshape(28,28))
+    plt.imshow(vars.reshape(28, 28))
     plt.colorbar()
     plt.show()
     return vars
-    
+
 
 vars = getVariances(digits)
 
@@ -75,15 +74,15 @@ def getCenterForDim(array2D, dim):
     totInk = 0
     for x in range(array2D.shape[0]):
         for y in range(array2D.shape[1]):
-            point = x,y
-            tot += point[dim]*array2D[x,y]
-            totInk += array2D[x,y]
-    return tot/totInk
+            point = x, y
+            tot += point[dim] * array2D[x, y]
+            totInk += array2D[x, y]
+    return tot / totInk
+
 
 def getCenter(array1D):
-    array2D = array1D.reshape(28,28)
-    return getCenterForDim(array2D,0), getCenterForDim(array2D,1)
-
+    array2D = array1D.reshape(28, 28)
+    return getCenterForDim(array2D, 0), getCenterForDim(array2D, 1)
 
 
 def extractCenters(pixData):
@@ -94,10 +93,11 @@ def extractCenters(pixData):
     X = []
     Y = []
     for sample in pixData:
-        x,y = getCenter(sample)
+        x, y = getCenter(sample)
         X.append(x)
         Y.append(y)
-    return np.array([X,Y]).T
+    return np.array([X, Y]).T
 
-
-XCents = extractCenters(pixs)
+# Extracting this feature takes quite some time and its usability is questionable
+# Uncomment next line if you want to extract it
+# XCents = extractCenters(digits)
