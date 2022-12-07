96f0fb077739a07df2b945dcd5313d0de43fa4cd
MarcoTurzi <56305514+MarcoTurzi@users.noreply.github.com>
2022-12-07 04:38:56 +0100

    added cross validation plot for both LR and SVC

--- trainingSVC_all_feature.py
+++ trainingSVC_all_feature.py
@@ -64,34 +64,68 @@ digits_scaled = scaler.fit_transform(new_digits)
 #train and test set split 
 x_train, x_test, y_train, y_test = train_test_split(digits_scaled, labels, test_size = 37/42, stratify=labels,shuffle=True)
 
-svc = SVC()
+svc = SVC(kernel="rbf")
+lr = LogisticRegression(penalty="l1", solver="saga", multi_class="multinomial",tol=0.001)
 
-dictionary = dict(C=uniform(loc=1,scale=2), kernel=['linear', 'poly', 'rbf'])
+dictionary = dict(C=uniform(loc=1,scale=2))
+dictionary_lr = dict(C=uniform(loc=1,scale=2) )
 
-cross = RandomizedSearchCV(svc,n_iter=20,n_jobs=4,param_distributions=dictionary,cv=10,verbose=3 ).fit(x_train,y_train)
+cross_SVC = RandomizedSearchCV(svc,n_iter=2,n_jobs=4,param_distributions=dictionary,cv=10,verbose=3 ).fit(x_train,y_train)
+
+cross_LR = RandomizedSearchCV(lr,n_iter=2,n_jobs=4,param_distributions=dictionary_lr,cv=10,verbose=3 ).fit(x_train,y_train)
 
 #cross = LogisticRegressionCV(verbose=3,n_jobs=4,cv=10,penalty="l1", solver="saga", multi_class="multinomial", max_iter=1000)
 
-cross.fit(x_train,y_train)
+lr = cross_LR.best_estimator_
+svc = cross_SVC.best_estimator_
 
-results = cross.cv_results_
+C_lr = cross_LR.cv_results_["param_C"]
+C_svc = cross_SVC.cv_results_["param_C"]
 
-# Get the test scores for each iteration
-test_scores = results['mean_test_score']
+score_lr = cross_LR.cv_results_["mean_test_score"]
+score_svc = cross_SVC.cv_results_["mean_test_score"]
 
-# Plot the results
-plt.plot(test_scores)
-plt.xlabel('Iteration')
-plt.ylabel('Test Score')
+dict_order_lr = {c:s for c,s in zip(C_lr, score_lr )}
+dict_order_svc = {c:s for c,s in zip(C_svc, score_svc)}
+
+
+dict_order_lr = dict(sorted(dict_order_lr.items(), key=lambda item: item[0]))
+dict_order_svc = dict(sorted(dict_order_svc.items(), key=lambda item: item[0]))
+
+
+plt.figure(figsize=(10,6))
+plt.plot([c for c,k in dict_order_lr.items()], [k for c,k in dict_order_lr.items()], label="Logistic Regression")
+plt.plot([c for c,k in dict_order_svc.items()],[k for c,k in dict_order_svc.items()], label="SVC")
+plt.xlabel("Value of parameter C")
+plt.ylabel("Average accuracy score")
+plt.title("Randomized Search Results")
+plt.legend()
 plt.show()
+# Get the test scores for each iteration
+'''plt.figure(figsize=(10, 6))
+plt.plot(range(1, len(logistic_results['mean_test_score'])+1), logistic_results['mean_test_score'], label="Logistic Regression")
+plt.plot(range(1, len(svc_results['mean_test_score'])+1), svc_results['mean_test_score'], label="SVC")
+plt.xlabel('Iteration')
+plt.ylabel('Mean Test Score')
+plt.title('Random Search Results')
+plt.legend()
+plt.show()'''
+
+lr.fit(x_train,y_train)
+svc.fit(x_train,y_train)
 
 
 #best classifier based on accuracy and confusion matrix
-print("Best params: "+str(cross.best_params_))
-print("Score", cross.score(x_test,y_test))
-print("Accuracy",accuracy_score(y_test, cross.predict(x_test)))
-plot_confusion_matrix(cross, x_test, y_test)
+print("SVC results:")
+print("Best params: "+str(cross_SVC.best_params_))
+print("Accuracy",accuracy_score(y_test, svc.predict(x_test)))
+plot_confusion_matrix(svc, x_test, y_test)
 plt.show()
 
+print("LR results:")
+print("Best params: "+str(cross_LR.best_params_))
+print("Accuracy",accuracy_score(y_test, lr.predict(x_test)))
+plot_confusion_matrix(lr, x_test, y_test)
+plt.show()
 
 
